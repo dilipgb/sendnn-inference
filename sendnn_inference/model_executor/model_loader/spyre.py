8fd504d405436562bf63b50fb5e5ff2430fe955c
Dilip Gowda Bhagavan <dilip.bhagavan@ibm.com>
2026-06-12 13:52:35 +0530

    add  model_config.quantization
    Signed-off-by: Dilip Gowda Bhagavan <dilip.bhagavan@ibm.com>

--- sendnn_inference/model_executor/model_loader/spyre.py
+++ sendnn_inference/model_executor/model_loader/spyre.py
@@ -247,7 +247,8 @@ class SpyreCausalLM(nn.Module):
         self.is_multimodal = self.mm_model_utils is not None
 
         if envs_spyre.SENDNN_INFERENCE_DYNAMO_BACKEND in BACKEND_LIST:
-            self._cast_params_for_spyre()
+            if not model_config.quantization:
+                self._cast_params_for_spyre()
             options = {"sendnn.dynamic": True} if sendnn_dynamic else {}
 
             # Lazy import to avoid load torch_sendnn runtime before it is really
